dd67fa5ad76af2f59b78bfc0c203967ee9801e44
megan <megan.schertz@gmail.com>
2021-10-01 17:57:50 -0500

    initial fetch request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    recipes: []
+  }
+  componentDidMount() {
+    fetch('http://localhost:3000/recipes')
+    .then(resp => resp.json())
+    .then(data => this.setState({recipes: data}))
+    .then(json => console.log(json))
+  }
+
+  render() {
+    console.log(this.state)
+    return(
+      <div>
+        {this.state.recipes.map(recipe => 
+        <div>
+          <h1>{recipe.name}</h1>
+          <h3>Rating: {recipe.rating} </h3>
+          <h3>Time</h3>
+          <p>{recipe.time}</p>
+          <h3>Servings</h3>
+          <p>{recipe.servings}</p>
+          <h3>Ingredients</h3>
+          <p>{recipe.ingredients}</p>
+          <h3>Directions</h3>
+          <p>{recipe.directions}</p>
+        </div>
+          )}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
